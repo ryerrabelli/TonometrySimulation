b1d9d71a1d4df9e7e158d386304fde302216356d
Rahul Yerrabelli <ryerrabelli@gmail.com>
2022-07-29 03:48:25 -0500

    UI

--- src/v02/GATsimulation.js
+++ src/v02/GATsimulation.js
@@ -46,7 +46,7 @@ var myGameArea = {
     this.canvas.width = 480;
     this.canvas.height = 360;
     this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.getElementById("GAT-game-controls"));
+    document.getElementById("GAT-game").insertBefore(this.canvas, document.getElementById("GAT-game-controls"));
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
     this.key = false;
--- src/v02/headshot.js
+++ src/v02/headshot.js
@@ -1,57 +1,59 @@
 // Original source: https://www.w3schools.com/howto/howto_js_image_zoom.asp
-function imageZoom(imgID, resultID) {
-  var img, lens, result, cx, cy;
-  img = document.getElementById(imgID);
-  result = document.getElementById(resultID);
-  /*create lens:*/
-  lens = document.createElement("div");
-  lens.setAttribute("class", "img-zoom-lens");
-  /*insert lens:*/
-  img.parentElement.insertBefore(lens, img);
-  /*calculate the ratio between result div and lens:*/
-  cx = result.offsetWidth / lens.offsetWidth;
-  cy = result.offsetHeight / lens.offsetHeight;
+function setUpPhotoZooming(origPhotoID, zoomedPhotoID) {
+  var origPhoto, zoomingLens, zoomedPhoto, cx, cy;
+  origPhoto = document.getElementById(origPhotoID);
+  zoomedPhoto = document.getElementById(zoomedPhotoID);
+
+  /*create  and insertzoomingLens:*/
+  zoomingLens = document.createElement("div");
+  zoomingLens.setAttribute("class", "img-zooming-lens");
+  /*insert zoomingLens:*/
+  origPhoto.parentElement.insertBefore(zoomingLens, origPhoto);
+  /*calculate the magnification by calculating the ratio between zoomedPhoto div (output) and zoomingLens (input):*/
+  cx = zoomedPhoto.offsetWidth / zoomingLens.offsetWidth;
+  cy = zoomedPhoto.offsetHeight / zoomingLens.offsetHeight;
 
   // values depend on the defined css values
   // orig 300 40 7.5 300 40 7.5
   // changed to 360 36 10 360 36 10
-  // console.log(result.offsetWidth, lens.offsetWidth, cx, result.offsetHeight, lens.offsetHeight, cy)
+  // console.log(zoomedPhoto.offsetWidth, zoomingLens.offsetWidth, cx, zoomedPhoto.offsetHeight, zoomingLens.offsetHeight, cy)
 
-
-  /*set background properties for the result DIV:*/
-  result.style.backgroundImage = "url('" + img.src + "')";
-  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-  /*execute a function when someone moves the cursor over the image, or the lens:*/
-  lens.addEventListener("mousemove", moveLens);
-  img.addEventListener("mousemove", moveLens);
+  /*set background properties for the zoomedPhoto DIV:*/
+  zoomedPhoto.style.backgroundImage = "url('" + origPhoto.src + "')";
+  zoomedPhoto.style.backgroundSize = (origPhoto.width * cx) + "px " + (origPhoto.height * cy) + "px";
+  /*execute a function when someone moves the cursor over the image, or the zoomingLens:*/
+  zoomingLens.addEventListener("mousemove", moveZoomingLens);
+  origPhoto.addEventListener("mousemove", moveZoomingLens);
   /*and also for touch screens:*/
-  lens.addEventListener("touchmove", moveLens);
-  img.addEventListener("touchmove", moveLens);
-  function moveLens(e) {
+  zoomingLens.addEventListener("touchmove", moveZoomingLens);
+  origPhoto.addEventListener("touchmove", moveZoomingLens);
+
+  function moveZoomingLens(e) {
     var pos, x, y;
     /*prevent any other actions that may occur when moving over the image:*/
     e.preventDefault();
     /*get the cursor's x and y positions:*/
     pos = getCursorPos(e);
-    /*calculate the position of the lens:*/
-    x = pos.x - (lens.offsetWidth / 2);
-    y = pos.y - (lens.offsetHeight / 2);
-    /*prevent the lens from being positioned outside the image:*/
-    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+    /*calculate the position of the zoomingLens:*/
+    x = pos.x - (zoomingLens.offsetWidth / 2);
+    y = pos.y - (zoomingLens.offsetHeight / 2);
+    /*prevent the zoomingLens from being positioned outside the image:*/
+    if (x > origPhoto.width - zoomingLens.offsetWidth) {x = origPhoto.width - zoomingLens.offsetWidth;}
     if (x < 0) {x = 0;}
-    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+    if (y > origPhoto.height - zoomingLens.offsetHeight) {y = origPhoto.height - zoomingLens.offsetHeight;}
     if (y < 0) {y = 0;}
-    /*set the position of the lens:*/
-    lens.style.left = x + "px";
-    lens.style.top = y + "px";
-    /*display what the lens "sees":*/
-    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+
+    /*set the position of the zoomingLens:*/
+    zoomingLens.style.left = x + "px";
+    zoomingLens.style.top = (zoomedPhoto.offsetHeight+y) + "px";
+    /*display what the zoomingLens "sees":*/
+    zoomedPhoto.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
   }
   function getCursorPos(e) {
     var a, x = 0, y = 0;
     e = e || window.event;
     /*get the x and y positions of the image:*/
-    a = img.getBoundingClientRect();
+    a = origPhoto.getBoundingClientRect();
     /*calculate the cursor's x and y coordinates, relative to the image:*/
     x = e.pageX - a.left;
     y = e.pageY - a.top;
