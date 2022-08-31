b6ed52cba95aa56adde966a6a9b41232bacdbfb8
Rahul Yerrabelli <ryerrabelli@gmail.com>
2022-08-30 21:12:02 -0500

    Refractored X and Y into Hor and Vert for the tonometry simulation (but not the underlying library) to avoid confusion later

--- src/v005/js/joystick.js
+++ src/v005/js/joystick.js
@@ -12,16 +12,16 @@ function setUpJoystick() {
   Joy = new JoyStick("joyDiv", joyParam);
 
 
-  const joyInputPosX = document.getElementById("joyPositionX");
-  const joyInputPosY = document.getElementById("joyPositionY");
+  const joyInputPosHor = document.getElementById("joyPositionHor");
+  const joyInputPosVer = document.getElementById("joyPositionVer");
   const joyDirection = document.getElementById("joyDirection");
-  const joyX = document.getElementById("joyX");
-  const joyY = document.getElementById("joyY");
+  const joyHor = document.getElementById("joyHor");
+  const joyVer = document.getElementById("joyVer");
 
-  setInterval(function(){ joyInputPosX.value=Joy.GetPosX().toFixed(1);; }, 50);
-  setInterval(function(){ joyInputPosY.value=Joy.GetPosY().toFixed(1);; }, 50);
+  setInterval(function(){ joyInputPosHor.value=Joy.GetPosX().toFixed(1);; }, 50);
+  setInterval(function(){ joyInputPosVer.value=Joy.GetPosY().toFixed(1);; }, 50);
   setInterval(function(){ joyDirection.value=Joy.GetDir(); }, 50);
-  setInterval(function(){ joyX.value=Joy.GetNormX().toFixed(4);; }, 50);
-  setInterval(function(){ joyY.value=Joy.GetNormY().toFixed(4); }, 50);
+  setInterval(function(){ joyHor.value=Joy.GetNormX().toFixed(4);; }, 50);
+  setInterval(function(){ joyVer.value=Joy.GetNormY().toFixed(4); }, 50);
 
 }
