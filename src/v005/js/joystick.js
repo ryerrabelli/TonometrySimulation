49d8280bbd393bdc7fa46b26c093d01caf93a34a
Rahul Yerrabelli <ryerrabelli@gmail.com>
2022-08-30 20:03:41 -0500

    Renaming and organizing

--- src/v005/js/joystick.js
+++ src/v005/js/joystick.js
@@ -1,18 +1,21 @@
 
 function setUpJoystick() {
-  // Create JoyStick object into the DIV 'joy2Div'
-  const joy2Param = {"title": "joystick2", "autoReturnToCenter": false};
-  const Joy2 = new JoyStick('joy2Div', joy2Param);
+  // Create JoyStick object into the DIV 'joyDiv'
+  const joyParam = {
+    "title": "joystick",
+    "autoReturnToCenter": false,
+  };
+  const Joy = new JoyStick("joyDiv", joyParam);
 
-  const joy2IinputPosX = document.getElementById("joy2PosizioneX");
-  const joy2InputPosY = document.getElementById("joy2PosizioneY");
-  const joy2Direzione = document.getElementById("joy2Direzione");
-  const joy2X = document.getElementById("joy2X");
-  const joy2Y = document.getElementById("joy2Y");
+  const joyIinputPosX = document.getElementById("joyPosizioneX");
+  const joyInputPosY = document.getElementById("joyPosizioneY");
+  const joyDirezione = document.getElementById("joyDirezione");
+  const joyX = document.getElementById("joyX");
+  const joyY = document.getElementById("joyY");
 
-  setInterval(function(){ joy2IinputPosX.value=Joy2.GetPosX(); }, 50);
-  setInterval(function(){ joy2InputPosY.value=Joy2.GetPosY(); }, 50);
-  setInterval(function(){ joy2Direzione.value=Joy2.GetDir(); }, 50);
-  setInterval(function(){ joy2X.value=Joy2.GetX(); }, 50);
-  setInterval(function(){ joy2Y.value=Joy2.GetY(); }, 50);
+  setInterval(function(){ joyIinputPosX.value=Joy.GetPosX(); }, 50);
+  setInterval(function(){ joyInputPosY.value=Joy.GetPosY(); }, 50);
+  setInterval(function(){ joyDirezione.value=Joy.GetDir(); }, 50);
+  setInterval(function(){ joyX.value=Joy.GetX(); }, 50);
+  setInterval(function(){ joyY.value=Joy.GetY(); }, 50);
 }
