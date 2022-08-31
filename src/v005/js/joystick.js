712d34d72a54eb8aa07633c3fdaa3d6cefd370bd
Rahul Yerrabelli <ryerrabelli@gmail.com>
2022-08-30 21:13:55 -0500

    Refractored again to add more detail

--- src/v005/js/joystick.js
+++ src/v005/js/joystick.js
@@ -15,13 +15,13 @@ function setUpJoystick() {
   const joyInputPosHor = document.getElementById("joyPositionHor");
   const joyInputPosVer = document.getElementById("joyPositionVer");
   const joyDirection = document.getElementById("joyDirection");
-  const joyHor = document.getElementById("joyHor");
-  const joyVer = document.getElementById("joyVer");
+  const joyNormHor = document.getElementById("joyHor");
+  const joyNormVer = document.getElementById("joyVer");
 
   setInterval(function(){ joyInputPosHor.value=Joy.GetPosX().toFixed(1);; }, 50);
   setInterval(function(){ joyInputPosVer.value=Joy.GetPosY().toFixed(1);; }, 50);
   setInterval(function(){ joyDirection.value=Joy.GetDir(); }, 50);
-  setInterval(function(){ joyHor.value=Joy.GetNormX().toFixed(4);; }, 50);
-  setInterval(function(){ joyVer.value=Joy.GetNormY().toFixed(4); }, 50);
+  setInterval(function(){ joyNormHor.value=Joy.GetNormX().toFixed(4);; }, 50);
+  setInterval(function(){ joyNormVer.value=Joy.GetNormY().toFixed(4); }, 50);
 
 }
